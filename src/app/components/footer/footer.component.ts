181b48d0fec8a3653823705426ba8a55ee9728ae
alex-millum <alexander@millum.no>
2026-01-21 20:41:36 +0100

    Add build version and date to footer component; update environment files

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { companyInfo } from '../../models/CompanyInfo.model';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-footer',
@@ -11,6 +12,8 @@ import { companyInfo } from '../../models/CompanyInfo.model';
 })
 export class FooterComponent {
   companyInfo = companyInfo;
+  version = environment.version;
+  buildDate = environment.buildDate;
 
   get mapsUrl(): string {
     const encodedAddress = encodeURIComponent(this.companyInfo.address);
