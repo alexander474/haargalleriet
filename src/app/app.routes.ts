de252dfc652210276b3042df4b88daf65ee0c306
alex-millum <alexander@millum.no>
2026-01-25 15:37:28 +0100

    fix: Update titles and canonical URLs for SEO optimization across multiple files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,27 +10,28 @@ export const appRoutes: Routes = [
   {
     path: '',
     component: HomepageComponent,
-    title: 'Hårgalleriet Fagernes | Frisørsalong i Valdres',
+    title:
+      'Hårgalleriet Fagernes | Frisørsalong i Valdres - Klipp, Farge & Styling',
   },
   {
     path: 'about',
     component: AboutComponent,
-    title: 'Om Oss | Hårgalleriet Fagernes',
+    title: 'Om Oss - Møt Våre Frisører | Hårgalleriet Fagernes',
   },
   {
     path: 'services',
     component: ServicesComponent,
-    title: 'Våre Tjenester | Hårgalleriet Fagernes',
+    title: 'Våre Tjenester - Klipp, Farge, Brud & Mer | Hårgalleriet Fagernes',
   },
   {
     path: 'prices',
     component: PricesComponent,
-    title: 'Priser | Hårgalleriet Fagernes',
+    title: 'Priser og Prisliste | Hårgalleriet Fagernes Frisørsalong',
   },
   {
     path: 'order',
     component: OrderComponent,
-    title: 'Bestill Time | Hårgalleriet Fagernes',
+    title: 'Bestill Time - Kontakt Oss | Hårgalleriet Fagernes',
   },
   {
     path: '**',
